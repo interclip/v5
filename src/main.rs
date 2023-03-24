f2a5d57c542689839fef1d36b78ed27b34c2b58c
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-24 21:19:56 +0100

    `get_db_clip` prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use mysql::prelude::*;
+use mysql::*;
+
+use std::result::Result;
+use std::result::Result::Ok;
+use std::string::String;
+
 #[macro_use]
 extern crate rocket;
 
@@ -13,6 +20,27 @@ fn index() -> &'static str {
     }
 }
 */
+
+static DB_URL: &str = "mysql://root:password@localhost:3307/db_name";
+
+fn get_db_clip(code: String) -> Result<String, mysql::Error> {
+    let pool = Pool::new(DB_URL)?;
+    let mut conn = pool.get_conn()?;
+    let query = format!("SELECT * FROM userurl WHERE usr = '{}'", code);
+    let result: Result<Vec<String>, mysql::Error> =
+        conn.query_map(query, |url| (url));
+
+    let result = match result {
+        Ok(result) => result.get(0).unwrap().to_string(),
+        Err(e) => {
+            println!("Error: {}", e);
+            return Err(e);
+        }
+    };
+
+    Ok(result.to_string())
+}
+
 #[get("/get?<code>")]
 fn get_clip(code: String) -> String {
     format!("get: {}", code)
