c108c2858304b66a4880c77bd5facc25010c128c
Filip Troníček <filip.tronicek@seznam.cz>
2023-04-01 20:09:59 +0200

    Add Redis utility scripts

--- src/utils.rs
+++ src/utils.rs
@@ -2,3 +2,4 @@ pub(crate) mod db;
 pub(crate) mod id;
 pub mod structs;
 pub mod rate_limit;
+pub mod redis;
---
+++ src/utils/redis.rs
@@ -0,0 +1,20 @@
+use redis::{Commands, Connection, RedisResult};
+
+static REDIS_URL: &str = "redis://localhost/";
+
+fn get_redis_conn() -> RedisResult<Connection> {
+    let client = redis::Client::open(REDIS_URL)?;
+    client.get_connection()
+}
+
+pub fn cache_clip(code: &str, url: &str) -> Result<(), redis::RedisError> {
+    let mut redis_conn = get_redis_conn()?;
+    let _: () = redis_conn.set_ex(code, url, 2592000)?; // Cache for 30 days (2592000 seconds)
+    Ok(())
+}
+
+pub fn get_cached_clip(code: &str) -> Result<Option<String>, redis::RedisError> {
+    let mut redis_conn = get_redis_conn()?;
+    let result: Option<String> = redis_conn.get(code)?;
+    Ok(result)
+}
