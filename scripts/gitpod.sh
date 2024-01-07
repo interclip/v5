2d230632a8d41fec14f080ea1650e9dbf2d73b7d
Filip Troníček <filip@gitpod.io>
2024-01-07 16:11:32 +0000

    Gitpod config

--- scripts/gitpod.sh
+++ scripts/gitpod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+docker pull postgres
+docker run --name pgdb -e POSTGRES_PASSWORD=development -p 5432:5432 -d postgres
+docker exec -it pgdb psql -U postgres -c "CREATE DATABASE interclip;"
+
 cp scripts/gitpod.env .env
